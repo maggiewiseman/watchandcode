fcb5a4e62d081c2b058f95d89fd9929b0e2cb5ce
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-12-06 08:21:42 -0500

    put all functions and list on an object

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-var todos = ["item1", "item2", "item3"];
 
-function displayTodos() {
-  for (var i=0; i < todos.length; i++) {
-    console.log( todos[i]);
+var todoList = {
+  todos: ["item1", "item2", "item3"],
+  displayTodos: function() {
+    console.log('My Todos: ', this.todos);
+  },
+  addTodo: function(item) {
+    this.todos.push(item);
+    this.displayTodos();
+  }, 
+  changeTodo: function(num, newTodo) {
+    if(this.todos[num]){
+      this.todos[num] = newTodo;
+      this.displayTodos();
+    }else {
+      console.log(num + "is an invalid number in your to do list.");
+    }
+  },
+  deleteTodo: function(position) {
+    if(this.todos[position]) {
+      this.todos.splice(position, 1);
+      this.displayTodos();
+    } else {
+      console.log(position + " is not on the list");
+    }
   }
   
-}
-
-function addTodo(item) {
-  todos.push(item);
-}
-
-function changeTodo(num, newTodo) {
-  if(todos[num]){
-    todos[num] = newTodo;
-    displayTodos();
-  }else {
-    console.log(num + "is an invalid number in your to do list.");
-  }
-}
-
-//remember slice deletes after the number provided, so if they want to delete 2, tell slice to delete after 1
-function deleteTodo(position) {
-  if(todos[position]) {
-    todos.splice(position, 1);
-    displayTodos();
-  } else {
-    console.log(position + " is not on the list");
-  }
-}
+};
 
 
-displayTodos();
-addTodo("copy tests");
-changeTodo(1, "grade labs");
-deleteTodo(2);
+
+////remember slice deletes after the number provided, so if they want to delete 2, tell slice to delete after 1
+//function deleteTodo(position) {
+//  
+//}
+//
+//
+//displayTodos();
+//addTodo("copy tests");
+//changeTodo(1, "grade labs");
+//deleteTodo(2);
 
 
