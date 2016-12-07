93e71169cbe275c72b3d8f0ad4b82ffea5514cf8
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-12-06 19:53:06 -0500

    updated todoList object with lots of methods to toggle completed, display, etc.

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 
 var todoList = {
-  todos: ["item1", "item2", "item3"],
+  todos: [],
   displayTodos: function() {
-    console.log('My Todos: ', this.todos);
+    if (this.todos.length === 0) {
+      console.log("you have no items on your list!");
+    } else {
+      for (var i = 0; i < this.todos.length; i++) {
+        var item = this.todos[i];
+        if(item.completed) {
+          console.log("(x) ", item.todoText);
+        } else {
+          console.log("( ) ", item.todoText);
+        }
+      }
+    }
   },
-  addTodo: function(item) {
-    this.todos.push(item);
+  addTodo: function(itemText) {
+    this.todos.push({
+      todoText: itemText,
+      completed: false
+    });
     this.displayTodos();
   }, 
   changeTodo: function(num, newTodo) {
     if(this.todos[num]){
-      this.todos[num] = newTodo;
+      this.todos[num].todoText = newTodo;
       this.displayTodos();
     }else {
       console.log(num + "is an invalid number in your to do list.");
@@ -23,9 +37,37 @@ var todoList = {
     } else {
       console.log(position + " is not on the list");
     }
+  },
+  toggleCompleted: function(num) {
+    this.todos[num].completed = !this.todos[num].completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var completedTodos = 0,
+        totalTodos = this.todos.length;
+    //count completed todos and total todos
+    for (var i = 0; i < this.todos.length; i++) {
+        if(this.todos[i].completed) { //completed is true so count it
+          completedTodos++;
+        }
+    }
+    console.log("completed todos: ", completedTodos);
+    if(completedTodos === totalTodos) {
+      //toggle all to false (remove check)
+      for (var j = 0; j < this.todos.length; j++) {
+        this.todos[j].completed = false;
+      }
+    } else {
+      //toggle all to true (add check)
+      for (var k = 0; k < this.todos.length; k++) {
+        this.todos[k].completed = true;
+      }
+    }
+    this.displayTodos();
   }
   
-};
+}; //end object
+
 
 
 
