782bf4d03535e75d353ca220a2465306a738262c
Maggie <maggie.wiseman@gmail.com>
2017-03-18 12:52:58 -0400

    working tests incl cross out, active button

--- tests/headlessTester.js
+++ tests/headlessTester.js
@@ -1,5 +1,5 @@
 // googletesting.js
-casper.test.begin('To Do List Tests', 5, function suite(test) {
+casper.test.begin('To Do List Tests', 9, function suite(test) {
   
 	
   //2 test items
@@ -22,7 +22,7 @@ casper.test.begin('To Do List Tests', 5, function suite(test) {
 			//take a picture of three items
 			//2 tests
 			console.log('take a pic, should be three items');
-				casper.capture('images/login.png');
+				casper.capture('images/threeItemsAdded.png');
 			casper.wait(500, function() {
 				test.assertEval(function() {
 						return __utils__.findAll("li").length === 3;
@@ -33,9 +33,32 @@ casper.test.begin('To Do List Tests', 5, function suite(test) {
     });
 	
 	  //remove a list item
-	  //1 test
+	  //2 tests
 		casper.then(function() {
 			test.assertExists('.destroy', 'destroy button exists');
+			this.click('.destroy', 'removed one item');
+			this.capture('images/twoItemsLeft.png');
+			test.assertTextDoesntExist('item 1', 'item 1 is gone from page');
+			
+		});
+	
+		//cross out a list item
+	  //1 test
+	  casper.then(function() {
+			test.assertExists('.complete-cb', 'complete check box exists');
+			this.click('.complete-cb', 'crossed off item 2');
+			this.capture('images/crossed1item.png');
+		});
+	
+		//click active and make sure there's only one item left
+		//2 tests
+		casper.then(function() {
+			test.assertExists('#active', 'active button exists');
+			this.click('#active', 'showing only active items');
+			this.wait(500, function(){
+				this.capture('images/activeItem.png');
+				test.assertTextDoesntExist('item 2', 'item 2 is gone from page');
+			});
 			
 		});
 
