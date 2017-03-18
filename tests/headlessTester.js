90e8c8e40e24fdb0e6595c6b234855c06def1393
Maggie <maggie.wiseman@gmail.com>
2017-03-18 11:47:33 -0400

    working tests, need to clear out local storage

--- tests/headlessTester.js
+++ tests/headlessTester.js
@@ -3,15 +3,18 @@ casper.test.begin('To Do List Tests', 4, function suite(test) {
     casper.start('file:///Users/maggie/Dropbox/codingFiles/javascript_practice/watchandcode/index.html', function() {
         test.assertTitle("Maggie's To Do App", "todo app title is the one expected");
         test.assertExists('#new-todo', "input field for new todo found");
-        this.fill('#header', {
-            newTodo: "casperjs test script"
-        }, true);
+			
     });
 
     casper.then(function() {
+			this.fill('#header', {
+            newTodo: "casperjs test script"
+        }, true);
+			this.sendKeys('#new-todo', casper.page.event.key.Enter);
+			console.log('about to take an image');
 			casper.capture('images/login.png');
 			test.assertTitle("Maggie's To Do App", "todo app title is the one expected");
-			casper.wait(1000, function() {
+			casper.wait(3000, function() {
 				test.assertEval(function() {
 						return __utils__.findAll("li").length === 	1;
 					}, "one list item added");
